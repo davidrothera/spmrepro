0733943c910a120311b801d7fa9e653132c07f54
David Rothera <david.rothera@gmail.com>
2020-01-19 11:14:09 +0000

    Initial Commit

---
+++ SPM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SPM
+//
+//  Created by David Rothera on 19/01/2020.
+//  Copyright © 2020 Supratude Limited. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
